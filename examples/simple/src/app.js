fc00318aeac360d23c7ebb164db173cca8eb4c2d
Francois Laberge <francois@jibo.com>
2017-01-22 21:17:55 -0500

    Some exploration testing the performance of even the smallest app

---
+++ examples/simple/src/app.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const h = require('react-hyperscript');
+const { div, span, h1 } = require('hyperscript-helpers')(h);
+
+module.exports = () => {
+
+	class MyComponent extends React.Component {
+	  render() {
+	    return div(`Hello World ${Math.random()}`);
+	  }
+	}
+
+	ReactDOM.render(h(MyComponent), document.getElementById('container') );
+}
